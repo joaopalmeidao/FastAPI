f85d9f3b6ff6882835a00bc1e23bc6732ef6f2d8
João Pedro Almeida Oliveira <96277609+joaopalmeidao@users.noreply.github.com>
2022-08-25 01:45:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ app = FastAPI()
 
 
 vendas = {
-    1:{'item':'lata','preco_unitario':4,'quantidade':5},
-    2:{'item':'garrafa 2L','preco_unitario':15,'quantidade':5},
-    3:{'item':'garrada 750ml','preco_unitario':10,'quantidade':5},
-    4:{'item':'lata mini','preco_unitario':2,'quantidade':5},
+    1: {'item': 'lata', 'preco_unitario': 4, 'quantidade': 5},
+    2: {'item': 'garrafa 2L', 'preco_unitario': 15, 'quantidade': 5},
+    3: {'item': 'garrada 750ml', 'preco_unitario': 10, 'quantidade': 5},
+    4: {'item': 'lata mini', 'preco_unitario': 2, 'quantidade': 5},
 }
+
 
 @app.get("/")
 def home():
     return vendas
 
+
 @app.get("/vendas/{id_venda}")
-def pegar_venda(id_venda:int):
+def pegar_venda(id_venda: int):
     return vendas[id_venda]
